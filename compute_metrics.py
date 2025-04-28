5687d8126994957a3ab6c1512786ff1a530a7b1b
Reggie Ashitey <149889408+RegAsh28@users.noreply.github.com>
2025-04-27 20:48:06 -0400

    Update compute_metrics.py (with hop metrics)

--- compute_metrics.py
+++ compute_metrics.py
@@ -16,6 +16,7 @@ def compute(all_packets, node_ip):
     
     request_times = {} #rtt
     rtts = []
+    hop_counts = []  # List to collect hop counts
     
     
     print(f"Node IP: {node_ip}")
@@ -28,10 +29,16 @@ def compute(all_packets, node_ip):
                 total_req_bytes_sent += packet["length"]
                 total_req_data_sent += (packet["length"] - 28)
                 request_times[packet["seq_num"]] = packet["time"]   #save time
+		if "ttl" in packet and packet["ttl"] is not None:
+                #Used the value of 129.67 because the TTL for all the packets are ~128-130
+                    hop_counts.append(129.67 - packet["ttl"])
             elif packet["destination"] == node_ip:
                 total_requests_received += 1
                 total_req_bytes_received += packet["length"]
                 total_req_data_received += (packet["length"] - 28)
+		if "ttl" in packet and packet["ttl"] is not None:
+                #Used the value of 129.67 because the TTL for all the packets are ~128-130
+                    hop_counts.append(129.67 - packet["ttl"])
 
         elif packet["type"] == 0:  # Echo Reply
             if packet["destination"] == node_ip:
@@ -56,6 +63,12 @@ def compute(all_packets, node_ip):
     else:
     	average_rtt = 0
     print(f"Average RTT (ms): {average_rtt:.2f}")
+    # Calculates the average echo request hop counts 
+    if hop_counts:
+        avg_hop_count = sum(hop_counts) / len(hop_counts)
+        print(f"Average Echo Request Hop Count: {avg_hop_count:.2f}")
+    else:
+        print("Average Echo Request Hop Count: N/A (no hop count data)")
 
 def main():
     filtered_file = sys.argv[1]
